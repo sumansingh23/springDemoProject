1e100ecb5cf78792996af19db2ec409123b3f2a0
Suman Singh <162127556+sumansingh23@users.noreply.github.com>
2024-07-24 19:02:29 +0530

    initial commitr

---
+++ src/test/java/com/example/springDemoProject/SpringDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springDemoProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
